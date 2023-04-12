b833c0ddc6fb5c165ee30c241eaf4f4ee62a58db
Happiness-James <gesarehjames@gmail.com>
2023-04-12 20:39:24 +0100

    added a login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,7 +4,12 @@ import React from "react";
 function Login(){
     return(
         <>
-        <h1>Login</h1>
+        <div className="card-login">
+        <h4>Welcome back!</h4>
+        <input className="form-control" type="text" placeholder="Enter Username" name="uname" required/><br/>
+        <input className="form-control" type="password" placeholder="Enter Password" name="psw" required/><br/>
+        <button className="btn login btn-primary" type="submit">Login</button>
+        </div>
         </>
     )
 }
