163a7a31f0a768118840f9ca36d227a2903192ef
Happiness-James <gesarehjames@gmail.com>
2023-04-12 21:04:21 +0100

    added a sign up form for both organisers and customers and styled them

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,7 +7,7 @@ function Login(){
         <>
         <div className="card-login">
         <h4>Welcome back!</h4>
-        <input className="form-control" type="text" placeholder="Enter Username" name="uname" required/><br/>
+        <input className="form-control" type="email" placeholder="Enter Your Email" name="uname" required/><br/>
         <input className="form-control" type="password" placeholder="Enter Password" name="psw" required/><br/>
         <button className="btn login btn-primary" type="submit">Login</button>
         </div>
