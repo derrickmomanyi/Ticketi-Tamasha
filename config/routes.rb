5487681ed1c43069bb4819828de18a94f73dc92a
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-04-13 23:58:27 +0300

    Added event controller and serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :organizers
   resources :customers
+  resources :events
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
   get '/me', to: 'organizers#show'
