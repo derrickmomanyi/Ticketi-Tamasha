44b6c592d92946dc92edce3fb4de45f372a65b24
Ian Gathui <ian.gathui@student.moringaschool.com>
2023-04-14 15:15:53 +0300

    migrated organizer_event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_080619) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_120357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_080619) do
     t.time "time"
     t.integer "tickets"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "organizer_events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
