09979e305bb505651cb0bf7ecbc4a929edc1be34
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-04-15 19:57:18 +0300

    Added ticket table details

--- client/src/components/EachEvent.js
+++ client/src/components/EachEvent.js
@@ -10,9 +10,9 @@ function EachEvent(){
     const { user } = useContext(UserContext) 
     const { id } = useParams(); 
     
-    const [earlyBirdCount, setEarlyBirdCount] = useState(0);
-    const [advanceCount, setAdvanceCount] = useState(0);
-    const [VIPCount, setVIPCount] = useState(0);
+    const [earlyBirdTicket, setEarlyBirdTicket] = useState(0);
+    const [advanceTicket, setAdvanceTicket] = useState(0);
+    const [VIPTicket, setVIPTicket] = useState(0);
 
     useEffect(() => {
         fetch(`/events/${id}`)
@@ -33,13 +33,24 @@ function EachEvent(){
         const VIPPrice = price * 4;
 
 
-        const earlyBirdSubtotal = earlyBirdCount * earlyBirdPrice;
-        const advanceSubtotal = advanceCount *  advancePrice;
-        const VIPSubtotal = VIPCount * VIPPrice;
-        const totalItems = earlyBirdCount + advanceCount + VIPCount;
+        const earlyBirdSubtotal = earlyBirdTicket * earlyBirdPrice;
+        const advanceSubtotal = advanceTicket *  advancePrice;
+        const VIPSubtotal = VIPTicket * VIPPrice;
+        const totalTickets = earlyBirdTicket + advanceTicket + VIPTicket;
         const totalPrice = earlyBirdSubtotal + advanceSubtotal + VIPSubtotal;
 
-      
+        const handleEarlyBirdChange = (event) => {
+            setEarlyBirdTicket(parseInt(event.target.value));
+          };
+
+        const handleAdvancedChange = (event) => {
+        setAdvanceTicket(parseInt(event.target.value));
+        };
+    
+        const handleVIPChange = (event) => {
+        setVIPTicket(parseInt(event.target.value));
+        };
+        
         
 
     return(
@@ -61,30 +72,84 @@ function EachEvent(){
                 <thead className="thead-dark">
                     <tr>
                     <th scope="col">Ticket</th>
-                    <th scope="col">Unit Cost</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Quantity</th>
                     <th scope="col">Total</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>Early Bird<br/>Closes on {date}</td>
-                        <td>Kshs {earlyBirdPrice}</td>
-                        <td>Kshs {earlyBirdSubtotal}</td>
+                        <td>Kshs {earlyBirdPrice.toFixed(2)}</td>
+                        <td>
+                            <select value={earlyBirdTicket} onChange={handleEarlyBirdChange}>
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>                            
+                            </select>
+                         </td>
+                        <td>Kshs {earlyBirdSubtotal.toFixed(2)}</td>
                     </tr>
                     <tr>                    
                         <td>Advance Tickets<br/>Closes on {date}</td>
-                        <td>Kshs {advancePrice}</td>
-                        <td>Kshs {advanceSubtotal}</td>
+                        <td>Kshs {advancePrice.toFixed(2)}</td>
+                        <td>
+                            <select value={advanceTicket} onChange={handleAdvancedChange}>
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>                            
+                            </select>
+                         </td>
+                        <td>Kshs {advanceSubtotal.toFixed(2)}</td>
                     </tr>
                     <tr>                    
                         <td>VIP<br/>Closes {date}</td>
-                        <td>Kshs {VIPPrice}</td>
-                        <td>Kshs {VIPSubtotal}</td>
+                        <td>Kshs {VIPPrice.toFixed(2)}</td>
+                        <td>
+                            <select value={VIPTicket} onChange={handleVIPChange}>
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>                            
+                            </select>
+                         </td>
+                        <td>Kshs {VIPSubtotal.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>TOTAL TICKETS<br/>{totalTickets} Tickets</td>                        
+                        <td>Kshs {totalPrice.toFixed(2)}</td>
                     </tr>
                 </tbody>
                 </table>
 
-                
+            </div>
+            <div className="payment_details">
+
             </div>
 
 
