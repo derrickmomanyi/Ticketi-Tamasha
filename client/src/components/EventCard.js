f22b5725e5d8b80155af38383bd0d16a2f3d5150
Happiness-James <gesarehjames@gmail.com>
2023-04-15 13:16:40 +0100

    rebasing

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -4,23 +4,47 @@ import '../css/EventCard.css';
 
 
 function EventCard({ event }) {
-    const {id, image, title, date, tickets} = event
+    const {image, title, date, location} = event
 
     function truncate(str, n) {
         return str?.length > n ? str.substr(0, n - 1) + "..." : str;
       }
 
+    const dates = new Date(date);
+    const dayInWords = dates.toLocaleString("default", { weekday: "long" }).slice(0,3) // "WED"
+    const day = dates.toLocaleString("default", { day: "numeric" })// "15"
+    const monthInWords = dates.toLocaleString("default", { month: "long" }).slice(0,3) // "APR"
+    
+
+    function getOrdinalSuffix(day) {
+        const j = day % 10, k = day % 100;
+        if (j === 1 && k !== 11) {
+          return "st";
+        }
+        if (j === 2 && k !== 12) {
+          return "nd";
+        }
+        if (j === 3 && k !== 13) {
+          return "rd";
+        }
+        return "th";
+      }
+
+
+         
     return (
         <>
-            <div className="card" style={{ width: '18rem'}}>
-                <img src={event.image} className="card-img-top"alt={event.image} />
-                <div className="card-body">
-                    <div>
-                        <p className="card-text">{date}</p>
-                    </div>
+            <div className="card " style={{ width: '18rem'}}>
+            <NavLink  to={`/events/${event.id}`}>
+                <img src={image} className="card-img-top"alt={image} />
+                </NavLink>
+                <div className="card-body">                    
                     <div>
                         <h5 className="card-title">{truncate(title, 20)}</h5>
-                        <p className="card-text">Available tickets:{tickets}</p>
+                        <div className="date-box">                       
+                            <span>{dayInWords}, {day}<sup>{getOrdinalSuffix(day)}</sup>, {monthInWords}</span>                        
+                        </div>
+                        <label>{location.toUpperCase()}</label>                        
                     </div>
 
 
