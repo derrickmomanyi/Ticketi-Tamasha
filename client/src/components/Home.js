1343f232807d1645a9a7e5528418d45d722f8b91
Happiness-James <gesarehjames@gmail.com>
2023-04-15 13:15:47 +0100

    rebasing

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,11 +5,9 @@ import '../css/Home.css';
 import EventCard from "./EventCard";
 
 
+
 function Home({ events, handleSearch, search }) {
     // const { user } = useContext(UserContext)
-    console.log(events);
-
-
 
     return (
         <>
@@ -17,31 +15,25 @@ function Home({ events, handleSearch, search }) {
                 <div className="transbox">
                     <div className="text-home">
                         <h1>An Event To Remember for Life!</h1>
-                        <p>Less work, more play. <br/> Whether you're into online streams, <br/> Weekend festivals or daytime get-togethers <br/> We have something for you. </p>
+                        <p>Less work, more play. <br /> Whether you're into online streams, <br /> Weekend festivals or daytime get-togethers <br /> We have something for you. </p>
                         <form action="" className="searchbar">
-                            <input type="search" className="form-control" required name="search" value={search} onChange={handleSearch} placeholder="Search event by category"></input>
+                            <input type="search" className="form-control icon" required name="search" value={search} onChange={handleSearch} placeholder="Search event by category"></input>
                         </form>
                     </div>
                 </div>
             </div><br />
 
-
             <div className="events-body">
                 <div className="card-heading col-md-12">
-                    <h2>Events</h2>
-                    <h5>Buy tickets in advance to popular events</h5>
+                    <h2 >Events in Kenya</h2>
+                    {/* <h5>Buy tickets in advance to popular events</h5> */}
                 </div>
                 <div className="row">
                     {events.map(event =>
-                    
-                        
-                            <EventCard key={event.id} event={event} />
-                       
-
+                        <EventCard key={event.id} event={event} />
                     )}
                 </div>
             </div>
-
         </>
     )
 }
