93ad7dc4583fa6a0d66135bf3cf1eceda4136276
Ian Gathui <ian.gathui@student.moringaschool.com>
2023-04-16 13:53:00 +0300

    added CRUD to OE and CE

--- app/controllers/customer_events_controller.rb
+++ app/controllers/customer_events_controller.rb
@@ -1,2 +1,38 @@
 class CustomerEventsController < ApplicationController
+    #permissions here
+    skip_before_action :authorized_user, :admin_user
+    wrap_parameters format: []
+
+    def show
+        customer_event = find_customer_event
+        render json: customer_event
+    end
+
+    def create 
+        customer_event = CustomerEvent.create!(customer_event_params)
+        render json: customer_event, status: :created
+    end
+
+    def update 
+        customer_event = CustomerEvent.find(params[:id])
+        customer_event.update!(customer_event_params)
+        render json: customer_event, status: :ok
+    end
+
+    def destroy
+        customer_event = find_customer_event
+        customer_event.destroy
+        head :no_content
+    end
+
+    private
+
+    def customer_event_params
+        params.permit(:event)
+    end
+
+    def find_customer_event
+        CustomerEvent.find(params[:id])
+    end
+    
 end
--- app/controllers/organizer_events_controller.rb
+++ app/controllers/organizer_events_controller.rb
@@ -1,2 +1,39 @@
 class OrganizerEventsController < ApplicationController
+    #permissions here
+    skip_before_action :authorized_user, :admin_user
+    wrap_parameters format: []
+
+
+    def show
+        organizer_event= find_organizer_event
+        render json: organizer_event
+    end
+
+    def create 
+        organizer_event = OrganizerEvent.create!(customer_event_params)
+        render json: organizer_event, status: :created
+    end
+
+    def update 
+        organizer_event = Organizer.find(params[:id])
+        organizer_event.update!(organizer_event_params)
+        render json: organizer_event, status: :ok
+    end
+
+    def destroy
+        organizer_event = find_organizer_event
+        organizer_event.destroy
+        head :no_content
+    end
+
+    private
+
+    def organizer_event_params
+        params.permit(:event)
+    end
+
+    def find_organizer_event
+        OrganizerEvent.find(params[:id])
+    end
+    
 end
