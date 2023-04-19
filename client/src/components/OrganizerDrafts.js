91e09d27bc732f9c27eab628264389c721f35563
Jesse <jessewambu@zetech.ac.ke>
2023-04-19 13:22:20 +0300

    patched

--- client/src/components/OrganizerDrafts.js
+++ client/src/components/OrganizerDrafts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useState} from "react";
 import { useParams } from 'react-router-dom'
 import OrganizerDraftsCard from "./OrganizerDraftsCard";
@@ -34,4 +35,43 @@ function OrganizerDrafts(){
         </div>
     )
 }
+=======
+import React, { useEffect, useState} from "react";
+import { useParams } from 'react-router-dom'
+import OrganizerDraftsCard from "./OrganizerDraftsCard";
+
+
+
+function OrganizerDrafts(){
+    const [drafts, setDrafts] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+    const { id } = useParams();
+    
+    useEffect(() => {
+        fetch(`/organizers/${id}/drafts`)
+        .then((res) => res.json())
+        .then((data) => {
+            setDrafts(data)
+            setIsLoaded(true)
+        })
+    }, [id])
+
+
+
+    function onDeleteDraft(id){
+        const updatedDrafts = drafts.filter(draft => draft.id !== id)
+            setDrafts(updatedDrafts)
+    }
+
+    if (!isLoaded) return <h2>Loading...</h2>
+    return(
+    
+        <div className="row" style={{marginLeft:"8%"}}>
+         {drafts.map((draft) => 
+        <OrganizerDraftsCard draft = {draft} key = {draft.id} draftId={ draft.id } onDeleteDraft={ onDeleteDraft }/>)}
+        
+        </div>
+    )
+}
+>>>>>>> 83386cc (Accepted incoming)
 export default OrganizerDrafts;
