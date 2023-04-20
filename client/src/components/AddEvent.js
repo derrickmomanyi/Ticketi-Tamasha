29f4fcccc23e57b755d34744e5a8a3a7c38ba439
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-04-20 18:31:28 +0300

    Accepted incoming

--- client/src/components/AddEvent.js
+++ client/src/components/AddEvent.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState} from "react";
-import '../css/AddEvent.css';
-import AddEventForm from "./AddEventForm";
-
-
-function AddEvent(){
-       
-       const [drafts, setDrafts] = useState([])
-
-       useEffect(() => {
-           fetch('/drafts')
-             .then((res) => res.json())
-             .then((data) => setDrafts(data))
-         }, [])
-   
-       
-      const handleAddDrafts = (newDraft) =>{
-        const updatedDraft = [...drafts, newDraft]
-        setDrafts(updatedDraft)  
-      }
-
-    
-    return(
-
-        <>
-        <AddEventForm onAddDrafts={ handleAddDrafts }/>
-        </>
-
-
-    )
-}
-=======
 
 import React, { useEffect, useState} from "react";
 import '../css/AddEvent.css';
@@ -63,5 +30,4 @@ function AddEvent(){
 
     )
 }
->>>>>>> origin/buffer
 export default AddEvent;
