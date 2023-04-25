15e47117388d56ed8691073de7c03c39a5b888f6
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-04-25 14:37:49 +0300

    Modified sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,23 @@ class SessionsController < ApplicationController
 
       # POST '/login'
         def create
-            organizer = Organizer.find_by(email: params[:email])
             customer = Customer.find_by(email: params[:email])
-        
-            if organizer&.authenticate(params[:password])
-            session[:user_id] = organizer.id
-            session[:is_organizer] = 1
-            render json: organizer, status: :ok
-    
-            elsif customer&.authenticate(params[:password])
+            organizer = Organizer.find_by(email: params[:email])
+           
+        if customer&.authenticate(params[:password])
             session[:user_id] = customer.id
             session[:is_organizer] = 0
             render json: customer, status: :ok
+        
+        elsif organizer&.authenticate(params[:password])
+        session[:user_id] = organizer.id
+        session[:is_organizer] = 1
+        render json: organizer, status: :ok   
+           
             
-            else
-            render json: { errors: 'Invalid Password or Username'}, status: :unauthorized
-            end
+        else
+        render json: { errors: 'Invalid Password or Username'}, status: :unauthorized
+        end
         
         end
 
